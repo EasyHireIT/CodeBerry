1e7f7f6032f0ced90a9b9d2481f983c1a4f68fb2
WojciechWalaszkowski <wojtekkw97@gmail.com>
2023-10-25 22:58:30 +0200

    Adding login, user page and hr page

--- CodeBerry/main/admin.py
+++ CodeBerry/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import WorkOffer, JobAnnouncement
 
-# Register your models here.
+admin.site.register(WorkOffer)
+admin.site.register(JobAnnouncement)
