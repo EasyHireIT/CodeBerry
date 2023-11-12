2b866352641fdd57b56b31f832311c55dd18642f
Adam W <adam.wonia@gmail.com>
2023-11-12 20:01:39 +0100

    Removed token

--- CodeBerry/CodeBerry/settings.py
+++ CodeBerry/CodeBerry/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gym3phu6m^u0j1@78sx-@=s$83h&_jrw-3-mn3%!0vmc(t@xcv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
