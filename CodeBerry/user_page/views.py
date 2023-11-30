5747c907e0cfa016af5e91cc1553c936a01ecdd4
Adam W <adam.wonia@gmail.com>
2023-11-30 20:27:48 +0100

    Migrations changed

--- CodeBerry/user_page/views.py
+++ CodeBerry/user_page/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import UserCreatorModel
 from .forms import UserCreatorForm
-from django.contrib.auth.models import User
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 
 
 # TODO
