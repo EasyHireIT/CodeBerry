219a0af0db5e847b47baff05eaef6a148d05478d
Adam W <adam.wonia@gmail.com>
2024-01-03 23:01:12 +0100

    Added new field to model and updated forms

--- CodeBerry/user_page/models.py
+++ CodeBerry/user_page/models.py
@@ -6,31 +6,22 @@ from django.core.validators import FileExtensionValidator
 # Model for CodeBerry User.
 class UserCreatorModel(models.Model):
     user_id = models.Index
-    user_name = models.CharField(max_length=50, blank=True, null=True, verbose_name="Imię i nazwisko")
-
-
-
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
-                                 message="Oczekiwany format numeru: +99999999999. Limit 15 cyfr")
-    user_phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True,
-                                         verbose_name="Numer telefonu", null=True)
-
-
-
-    user_email = models.EmailField(max_length=30, null=True, verbose_name="Adres e-mail", blank=True)
+    user_name = models.CharField(max_length=50, blank=True, null=True)
+    user_phone_number = models.CharField(max_length=15, blank=True, null=True)
+    user_email = models.EmailField(max_length=30, null=True, blank=True)
     user_education = models.CharField(max_length=50, blank=True, null=True)
-    user_work_period = models.CharField(max_length=50, blank=True, null=True)
-    user_employment_type = models.CharField(max_length=50, blank=True, null=True)
+    user_notice_period = models.CharField(max_length=50, blank=True, null=True)
+    user_work_model = models.CharField(max_length=50, blank=True, null=True)
     user_relocation = models.CharField(max_length=50, blank=True, null=True)
-    user_employment_form = models.CharField(max_length=50, blank=True, null=True)
+    user_relocation_cities = models.CharField(max_length=100, blank=True, null=True)
+    user_work_contract = models.CharField(max_length=50, blank=True, null=True)
     user_contact = models.CharField(max_length=50, blank=True, null=True)
     user_languages = models.CharField(max_length=200, blank=True, null=True)
     user_skills = models.CharField(max_length=200, blank=True, null=True)
     user_salary = models.DecimalField(max_digits=5, decimal_places=0, blank=True, null=True)
-    user_bio = models.TextField(blank=True, null=True, verbose_name="Bio")
-    user_repo_link = models.CharField(max_length=80, blank=True, null=True,
-                                      verbose_name="Link do repozytorium z projektami")
-    user_linkedin = models.CharField(max_length=80, blank=True, null=True, verbose_name="Twój Linkedin")
+    user_bio = models.TextField(blank=True, null=True)
+    user_repo_link = models.CharField(max_length=80, blank=True, null=True)
+    user_linkedin = models.CharField(max_length=80, blank=True, null=True)
     user_cv = models.FileField(null=True, blank=True, upload_to=r'files/',
                                validators=[FileExtensionValidator(allowed_extensions=["pdf"])])
 
